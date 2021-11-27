e61a0d50772adf49cabba442472ce44e96399d9f
Your Name <mahtab.salahshoor@gmail.com>
2021-11-26 23:40:57 -0800

    pushing the final changes

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 function rpsGame(yourChoice) {
   console.log(yourChoice);
   var humanChoice, botChoice;
-  humanChoice = yourChoice.id;
-  botChoice = numberToChoice();
-  console.log("computer choice:", botChoice);
+  humanChoice = yourChoice;
+
+  botChoice = numberToChoice(randToRpsInt());
+  console.log("computerChoice", botChoice);
+
   results = decideWinner(humanChoice, botChoice);
   console.log(results);
+
   message = finalMessage(results);
   console.log(message);
+  rpsFrontEnd(yourChoice.id, botChoice, message);
 }
-
-const randToRpsInt = Math.floor(Math.random() * 3);
+function randToRpsInt() {
+  const randToRpsInt = Math.floor(Math.random() * 3);
+}
 console.log(randToRpsInt);
 
 function numberToChoice(number) {
@@ -26,15 +31,16 @@ function decideWinner(yourChoice, computerChoice) {
   var yourScore = rpsDatabase[yourChoice][computerChoice];
   var computerScore = rpsDatabase[computerChoice][yourChoice];
 
-  return [yourScore];
+  return [yourScore, computerScore];
 }
 function finalMessage([yourScore, computerScore]) {
-  if (yourScore === 0) {
-    return { message: "you lost", color: "red" };
-  } else if (yourScore === 0.5) {
-    return { message: "you tied", color: "yellow" };
-  } else {
-    return { message: "you won", color: "green" };
+  switch (true) {
+    case yourScore > computerScore:
+      return { message: "You won!", color: "green" };
+    case computerScore > yourScore:
+      return { message: "You lost!", color: "red" };
+    default:
+      return { message: "You tied!", color: "yellow" };
   }
 }
 function rpsFrontEnd(humanImageChoice, botImageChoice, finalMessage) {
@@ -51,6 +57,21 @@ function rpsFrontEnd(humanImageChoice, botImageChoice, finalMessage) {
   var humanDiv = document.createElement("div");
   var botDiv = document.createElement("div");
   var messageDiv = document.createElement("div");
-  humanDiv.innerHTML = "<img src='" + imagesDatabase[humanImageChoice] + "' >";
+  humanDiv.innerHTML =
+    "<img src='" +
+    imagesDatabase[humanImageChoice] +
+    "'min-width:280px width=400px style='box-shadow: 0px 10px 50px rgba(37, 50, 233, 1); '>";
+  messageDiv.innerHTML =
+    "<h1 style= 'color:" +
+    finalMessage["color"] +
+    "; font-size: 60px; padding: 30px; '>" +
+    finalMessage["message"] +
+    "</h1>";
+  botDiv.innerHTML =
+    "<img src='" +
+    imagesDatabase[botImageChoice] +
+    "'min-width:280px width=400px style='box-shadow: 0px 10px 50px rgba(243, 38, 24, 1); '>";
   document.getElementById("flex-box-rps-div").appendChild(humanDiv);
+  document.getElementById("flex-box-rps-div").appendChild(botDiv);
+  document.getElementById("flex-box-rps-div").appendChild(messageDiv);
 }
